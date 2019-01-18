be001f91a6c6c4d59e3a17be94c37b09cf7461d9
Aya Hatakeyama <0730sylph@gmail.com>
2019-01-18 07:32:49 +0000

    users_first_commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,24 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+  	@user = User.find(params[:id])
   end
 
   def update
+  	@user = User.find(params[:id])
+  	if @user.update(user_params)
+  		flash[:notice] = "プロフィールを編集しました"
+  		redirect_to user_path(@user.id)
+  	else
+  		render 'edit'
+  	end
   end
+
+  private
+
+  def user_params
+  	params.require(:user).permit(:name, :name_kana, :email, :phone_number, :nickname, :user_description, :user_image)
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'users/show'
-  get 'users/edit'
-  get 'users/update'
+  resources :users
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'tops#top'
