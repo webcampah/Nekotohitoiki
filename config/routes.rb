3ae3f3254c07019f2f0cdf10079ee71bb1df51b8
Aya Hatakeyama <0730sylph@gmail.com>
2019-01-19 06:30:47 +0000

    Favorites_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'favorites/create'
+  get 'favorites/destroy'
+  get 'favorites/index'
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'tops#top'
