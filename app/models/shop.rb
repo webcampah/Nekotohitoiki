f87144bce12ccd57b87d691a4127bfeb6dbfa7c6
Aya Hatakeyama <0730sylph@gmail.com>
2019-01-22 04:01:07 +0000

    アソシエーション途中

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,2 +1,16 @@
 class Shop < ApplicationRecord
+  attachment :shop_image
+
+  has_many :comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  has_many :users, through: :favorites, source: :user
+  belongs_to :prefecture
+
+  validates :shop_name, presence: true, length: {maximum: 100}
+  validates :address, presence: true, length: {maximum: 100}
+  validates :shop_description, length: {maximum: 400}
+
+  def fav_user(user_id)
+  	Favorite.find_by(user_id: user_id)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   attachment :user_image
+
+  has_many :comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  has_many :shops, through: :favorites, source: :shop
+
+  validates :name, presence: true, length: {in: 2..20}
+  validates :email, presence: :true
+  validates :nickname, presence: true, length: {in: 2..20}
 end
