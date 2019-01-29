8e49f3e00c7afef226ce6cdfc4d4956cf31fabbf
Aya Hatakeyama <0730sylph@gmail.com>
2019-01-29 08:40:51 +0000

    first_test

--- spec/controllers/shops_controller_spec.rb
+++ spec/controllers/shops_controller_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe ShopsController, type: :controller do
+	describe '詳細ページ' do
+		context "詳細ページが正しく表示される" do
+			before do
+				get :new
+			end
+			it 'リクエストは200 OKとなること' do
+				expect(response.status).to eq 200
+			end
+		end
+	end
 
 end
