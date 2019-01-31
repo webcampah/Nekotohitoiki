d2fb837a4466af7bbacd21df02e49a73f1d0fe3b
Aya Hatakeyama <0730sylph@gmail.com>
2019-01-31 05:02:40 +0000

    adminログイン機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   devise_for :users
   get '/users', to: redirect("/users/sign_up")
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get    '/admins/sign_in',  to: 'admins/sessions#new',     as: 'new_admin_session'
+  post   '/admins/sign_in',  to: 'admins/sessions#create',  as: 'admin_session'
+  delete '/admins/sign_out', to: 'admins/sessions#destroy', as: 'destroy_admin_session'
+
   root 'tops#top'
   resources :users do
   	get 'favs', to: 'favorites#index'
