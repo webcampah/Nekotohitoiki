d389e34b6fe773a34464e8fb755e0772d0346916
Aya Hatakeyama <0730sylph@gmail.com>
2019-02-02 05:49:37 +0000

    不要なマイグレーションファイル 削除

--- db/migrate/20190128051052_delete_prefectures.rb
+++
@@ -1,5 +0,0 @@
-class DeletePrefectures < ActiveRecord::Migration[5.2]
-  def change
-  	drop_table :prefectures
-  end
-end
--- db/migrate/20190128051333_remove_prefecture_id_from_shops.rb
+++
@@ -1,6 +0,0 @@
-class RemovePrefectureIdFromShops < ActiveRecord::Migration[5.2]
-  def change
-    remove_index :shops, :prefecture_id
-    remove_column :shops, :prefecture_id, :string
-  end
-end
